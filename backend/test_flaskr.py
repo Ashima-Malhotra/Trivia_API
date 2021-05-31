e3c84e28b3e1fc1a73b3a67604da16e6321b3a5e
Ashima Malhotra <am51730@PruCapODC.local>
2021-05-31 12:30:11 +0100

    commit

---
+++ backend/test_flaskr.py
@@ -0,0 +1,161 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgresql://{}:{}/{}".format('postgres','user@localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        self.new_question = {
+            'question': 'What is the capital of Maharashtra?',
+            'answer': 'Mumbai',
+            'difficulty': 5,
+            'category': 3
+        }
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+    def test_retrieve_categories(self):
+        res = self.client().get('/categories/')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+
+    def test_retrieve_questions(self):
+        res = self.client().get('/questions/')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])   
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
+
+    def test_delete_questions(self):
+        res = self.client().delete('/questions/9')
+        data = json.loads(res.data)
+        question = Question.query.filter(Question.id == 9).one_or_none()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 9)  
+
+    def test_add_question(self):
+        res = self.client().post('/questions/', json=self.new_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])    
+
+    def test_404_sent_requesting_questions_beyond_valid_page(self):
+        res = self.client().get('/questions/?page=100')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')  
+
+    def test_404_sent_requesting_non_existing_category(self):
+        res = self.client().get('/categories/88')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')  
+
+    def test_422_sent_deleting_non_existing_question(self):
+        res = self.client().delete('/questions/660')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')    
+
+    def test_422_add_question(self):
+        question = {
+            'question': 'What is the capital of Maharashtra?',
+            'answer': 'Mumbai',
+            'difficulty': 5,
+        }
+        res = self.client().post('/questions/', json=question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")
+
+    def test_search_questions(self):
+        new_search = {'searchTerm': 'x'}
+        res = self.client().post('/questions/search/', json=new_search)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone(data['questions'])
+        self.assertIsNotNone(data['total_questions'])
+
+    def test_404_search_questions(self):
+        new_search = {
+            'searchTerm': '',
+        }
+        res = self.client().post('/questions/search/', json=new_search)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")  
+
+    def test_get_questions_by_category(self):
+        res = self.client().get('/categories/5/questions/')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['currentCategory'])
+
+    def test_404_get_questions_by_category(self):
+        res = self.client().get('/categories/trivia/questions/')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_play_quiz(self):
+        new_quiz_round = {'previous_questions': [],
+                          'quiz_category': {'type': 'Entertainment', 'id': 5}}
+        res = self.client().post('/quizzes/', json=new_quiz_round)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_404_play_quiz(self):
+        new_quiz_round = {'previous_questions': []}
+        res = self.client().post('/quizzes/', json=new_quiz_round)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")      
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
---
+++ frontend/build/precache-manifest.8d8521a2d6bf527cd08d7a8062d5c1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2839f79d96e1d755a0abc2c244b311f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d895cdc7a3be526a8a6e",
+    "url": "/static/css/main.ee25cf69.chunk.css"
+  },
+  {
+    "revision": "072f3e95e3a0453af94c",
+    "url": "/static/js/2.6d5d0db4.chunk.js"
+  },
+  {
+    "revision": "d895cdc7a3be526a8a6e",
+    "url": "/static/js/main.0ad44879.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
